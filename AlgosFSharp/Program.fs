4fbae89c112734137514db2a523f673794ab6f6d
unknown <vdimitrov13@yahoo.com>
2022-04-03 15:57:53 +0300

    additional exercises

--- AlgosFSharp/Program.fs
+++ AlgosFSharp/Program.fs
@@ -1,5 +1,5 @@
 ﻿let calculateWordScore (word: string) =
-    word.Replace("a", "").Length;
+    word.Replace("a", "").Length
 
 let calculateBonus (word: string) =
     if word.Contains('c') then 5 else 0
@@ -7,5 +7,18 @@ let calculateBonus (word: string) =
 let calculatePenalty (word: string) =
     if word.Contains('s') then 7 else 0
 
+let sortWordsByLength (word: string) =
+    word.Length
+
+let sortWordsByLetterS (word: string) =
+    Array.filter(fun (x: char) -> x.Equals('s')) (word.ToCharArray()) 
+    |> fun (x) -> (x.Length)
+
+let sortNumbersAscending (numbers: int[]) =
+    Array.sortBy(fun (x :int) -> (x)) numbers
+
+let sortNumbersDescending (numbers: int[]) =
+    Array.sortByDescending(fun (x :int) -> (x)) numbers
+
 let rankWordsByScore(words : string[], sortingFunction : string -> int) = 
-     Array.sortByDescending(fun (x :string) -> sortingFunction(x)) words
+    Array.sortByDescending(fun (x :string) -> sortingFunction(x)) words
