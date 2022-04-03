b2b9ec685efe5748c9fc74942b99c5611bddf6c9
unknown <vdimitrov13@yahoo.com>
2022-04-03 12:44:10 +0300

    fsharp

---
+++ AlgosFSharp/Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let calculateWordScore (word: string) =
+    word.Replace("a", "").Length;
+
+let calculateBonus (word: string) =
+    if word.Contains('c') then 5 else 0
+
+let calculatePenalty (word: string) =
+    if word.Contains('s') then 7 else 0
+
+let rankWordsByScore(words : string[], sortingFunction) = 
+     Array.sortByDescending(fun (x :string) -> sortingFunction(x)) words
